bd16c85c8248a57702a712ff7134b0ab9d5e530e
0e1 <0e1@riseup.net>
2025-07-06 12:35:36 -0300

    Uniformiza parametros. osm-codes/WS#58 osm-codes/swagger#1

--- src/step01geoUriExt-api.sql
+++ src/step01geoUriExt-api.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE SCHEMA    IF NOT EXISTS api;
 
 CREATE or replace FUNCTION api.olc_encode(
-  uri text
+  p_uri text
 ) RETURNS jsonb AS $f$
   SELECT jsonb_build_object(
     'type', 'FeatureCollection',
@@ -21,21 +21,21 @@ CREATE or replace FUNCTION api.olc_encode(
         LATERAL (SELECT geouri_ext.olc_geom(x)) s(y)
       )
     )
-  FROM ( SELECT str_geouri_decode(uri) ) t(u)
+  FROM ( SELECT str_geouri_decode(p_uri) ) t(u)
 $f$ LANGUAGE SQL IMMUTABLE;
 COMMENT ON FUNCTION api.olc_encode(text)
-  IS 'Encodes Geo URI to OLC.'
+  IS 'Encodes GeoURI to OLC.'
 ;
 -- EXPLAIN ANALYZE SELECT api.olc_encode('geo:olc:-23.550385,-46.633956;u=11');
 
 CREATE or replace FUNCTION api.olc_decode(
-  code text
+  p_code text
 ) RETURNS jsonb AS $f$
 BEGIN
   RETURN
   (
     SELECT
-      CASE geouri_ext.olc_isfull(code)
+      CASE geouri_ext.olc_isfull(p_code)
         WHEN FALSE THEN jsonb_build_object('error', 'Not a valid full code.')
         WHEN TRUE THEN
         (
@@ -44,7 +44,7 @@ BEGIN
             'features', jsonb_agg(
                   ST_AsGeoJSONb(y,8,0,null,
                       jsonb_build_object(
-                          'code', code,
+                          'code', p_code,
                           'type', 'olc',
                           'area', ST_Area(y,true),
                           'side', SQRT(ST_Area(y,true))
@@ -54,7 +54,7 @@ BEGIN
         )
         ELSE jsonb_build_object('error', 'Unknown.')
       END
-    FROM (SELEcT geouri_ext.olc_geom(code)) s(y)
+    FROM (SELEcT geouri_ext.olc_geom(p_code)) s(y)
   );
 END;
 $f$ LANGUAGE 'plpgsql' IMMUTABLE;
@@ -66,7 +66,7 @@ COMMENT ON FUNCTION api.olc_decode(text)
 --------------------
 
 CREATE or replace FUNCTION api.ghs_encode(
-  uri text
+  p_uri text
 ) RETURNS jsonb AS $f$
   SELECT jsonb_build_object(
     'type', 'FeatureCollection',
@@ -85,16 +85,16 @@ CREATE or replace FUNCTION api.ghs_encode(
         LATERAL (SELECT ST_GeomFromGeoHash(x)) s(y)
       )
     )
-  FROM ( SELECT str_geouri_decode(uri) ) t(u)
+  FROM ( SELECT str_geouri_decode(p_uri) ) t(u)
 $f$ LANGUAGE SQL IMMUTABLE;
 COMMENT ON FUNCTION api.ghs_encode(text)
-  IS 'Encodes Geo URI to GHS.'
+  IS 'Encodes GeoURI to GHS.'
 ;
 -- EXPLAIN ANALYZE SELECT api.ghs_encode('geo:ghs:-23.550385,-46.633956;u=11');
 
 
 CREATE or replace FUNCTION api.ghs_decode(
-  code text,
+  p_code text,
   digits integer DEFAULT NULL -- truncate when not null
 ) RETURNS jsonb AS $f$
   SELECT jsonb_build_object(
@@ -102,14 +102,14 @@ CREATE or replace FUNCTION api.ghs_decode(
     'features', jsonb_agg(
           ST_AsGeoJSONb(y,8,0,null,
               jsonb_build_object(
-                  'code', code,
+                  'code', p_code,
                   'type', 'ghs',
                   'area', ST_Area(y,true),
                   'side', SQRT(ST_Area(y,true))
                   )
               )::jsonb)
     )
-    FROM (SELECT geouri_ext.ghs_geom(code)) s(y)
+    FROM (SELECT geouri_ext.ghs_geom(p_code)) s(y)
 $f$ LANGUAGE SQL IMMUTABLE;
 COMMENT ON FUNCTION api.ghs_decode(text,integer)
   IS 'Decodes GHS.'
